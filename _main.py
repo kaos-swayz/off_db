03b75e3a56d60eb61189348f914eff5282ecdce3
Swojak-A <swojak.a@gmail.com>
2018-02-02 23:18:09 +0100

    add writing to file to fetch_links

--- _main.py
+++ _main.py
@@ -2,7 +2,7 @@ import requests
 
 import bs4 as bs
 
-file_name1 = "url_data.txt"
+
 
 def unpack_url_data(file_name):
     """ pattern = url_data[0] - base url, url_data[1] - ext url """
@@ -18,13 +18,16 @@ def fetch_soup(url):
     soup = bs.BeautifulSoup(html, "lxml")
     return soup
 
-def fetch_links(soup):
+def fetch_links(soup, output_file_name="output_urls.txt"):
     for link in soup.find_all("a", {"class": "rmb-object-list-item"}):
-        print("".join(url_data[0] + link.get("href")))
+        url = "".join(url_data[0] + link.get("href"))
+        with open(output_file_name, "a", encoding="UTF-8") as fp:
+            fp.write(url + "\n")
 
 def parse_by_pages(url_data, min_page=0, max_page=9999):
-    url = "".join(url_data) + str(min_page)
-    fetch_links(fetch_soup(url))
+    for i in range(min_page, max_page):
+        url = "".join(url_data) + str(i)
+        fetch_links(fetch_soup(url))
 
 
 def test_connection(soup):
@@ -33,10 +36,13 @@ def test_connection(soup):
     print(soup.title.string)
 
 if __name__ == "__main__":
+    input_file_name1 = "url_data.txt"
+    output_urls = "output_urls.txt"
+
     url_data = unpack_url_data("url_data.txt")
     # print("".join(url_data))
 
-    parse_by_pages(url_data)
+    parse_by_pages(url_data, min_page=0, max_page=3)
 
 
     # test_connection(soup)
