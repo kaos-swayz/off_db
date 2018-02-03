a7e26683f7fd0cc031f75f58c70461fbf27617ac
Swojak-A <swojak.a@gmail.com>
2018-02-03 15:46:33 +0100

    add fetch_element and parse_by_links

--- rm_parser.py
+++ rm_parser.py
@@ -4,6 +4,8 @@ import requests
 
 import bs4 as bs
 
+
+
 def fetch_links(soup, output_file_name="urls_output.txt"):
     for link in soup.find_all("a", {"class": "rmb-object-list-item"}):
         url = "".join(url_data[0] + link.get("href"))
@@ -11,12 +13,21 @@ def fetch_links(soup, output_file_name="urls_output.txt"):
         with open(output_file_name, "a", encoding="UTF-8") as fp:
             fp.write(url + "\n")
 
-def parse_by_pages(url_data, min_page=0, max_page=9999):
+def parse_by_pages(url_data_list, min_page=0, max_page=9999):
     for i in range(min_page, max_page):
         # code generates url by joining all parts of url_data list
-        url = "".join(url_data) + str(i)
+        url = "".join(url_data_list) + str(i)
         fetch_links(fetch_soup(url),output_urls)
 
+
+
+def fetch_element(soup):
+    for e in soup.find_all("div", {"class": "rmb-details-list-item"}):
+        print(e.string)
+
+def parse_by_links(url_data_list):
+    for e in url_data_list:
+        pass
 
 
 
