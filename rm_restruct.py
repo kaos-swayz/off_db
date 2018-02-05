2c0b46c8e6363d653c87dcc23f8a20f698ef78e2
Swojak-A <swojak.a@gmail.com>
2018-02-05 18:52:44 +0100

    add check_count

--- rm_restruct.py
+++ rm_restruct.py
@@ -8,17 +8,34 @@ def open_data(file_name):
 
 def restruct_data(data):
     output = []
-    for e in data:
-        item = {}
-        output.append(item)
+
     return output
 
 
 
+def check_count(data):
+    len_41 = 0
+    len_43 = 0
+    len_inne = 0
+    for e in data:
+        # print(len(e))
+        if len(e) == 41:
+            len_41 += 1
+        elif len(e) == 43:
+            len_43 += 1
+        else:
+            len_inne += 1
+            print(len(e))
+            print(e)
+    print("len 41: {}, lend 43: {}, inne: {}".format(len_41, len_43, len_inne))
 
 
 
 if __name__ == "__main__":
     data = open_data("raw_data.txt")
+    print(data[0])
+
+    check_count(data)
+
     restructed_data = restruct_data(data)
     print(len(restructed_data))
