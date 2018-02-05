68d1f05971f4c8686cda9f1b80b48542fa339ce5
Swojak-A <swojak.a@gmail.com>
2018-02-05 19:04:30 +0100

    ext restruct_data (add "01.main_data")

--- rm_restruct.py
+++ rm_restruct.py
@@ -10,9 +10,18 @@ def restruct_data(data):
     output = []
     for e in data:
         item = {}
+
+        item["01.main_data"] = {}
+        item["01.main_data"]["name"] = e[1]
+        if "podnajem" not in e[1]:
+            item["01.main_data"]["type"] = ["lease"]
+        else:
+            item["01.main_data"]["type"] = ["lease"]
+
         item["09.metadata"] = {}
         item["09.metadata"]["remobile_id"] = e[0]
         item["09.metadata"]["remobile_url"] = e[2]
+        item["09.metadata"]["remobile_pic_url"] = e[3]
 
 
         output.append(item)
@@ -36,13 +45,20 @@ def check_count(data):
             print(e)
     print("len 41: {}, lend 43: {}, inne: {}".format(len_41, len_43, len_inne))
 
-
+def check_podnajem(data):
+    podnajem_count = 0
+    for e in data:
+        if "podnajem" in e[1]:
+            podnajem_count += 1
+            print(e)
+    print("podnajem_count: {}".format(podnajem_count))
 
 if __name__ == "__main__":
     data = open_data("raw_data.txt")
     print(data[0])
 
-    check_count(data)
+    # check_count(data)
+    # check_podnajem(data)
 
     restructed_data = restruct_data(data)
     print(len(restructed_data))
