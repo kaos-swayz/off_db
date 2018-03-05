2ed8cc428eda9c004179fded3e56bbf6aab07d56
Swojak-A <swojak.a@gmail.com>
2018-03-05 18:42:21 +0100

    add new saving system: corrections function (corrections_matching_data, corrections_modify_city)

--- restruct_ii.py
+++ restruct_ii.py
@@ -2,6 +2,7 @@ from _main import open_json_file, save_json_file
 
 from os import listdir
 from difflib import SequenceMatcher
+import jellyfish
 from time import sleep
 
 class Restructor_ii:
@@ -23,8 +24,8 @@ class Restructor_ii:
                 for item in file_data:
                     output_data[id] = item
                     output_data[id]["01.main_data"]["id"] = id
-                    output_data[id]["01.main_data"]["match_id"] = ""
-                    output_data[id]["01.main_data"]["match_level"] = ""
+                    self.corrections_matching_data(output_data[id])
+                    self.corrections_unify_city(output_data[id])
                     id += 1
 
         save_json_file(file_name=output_file_name, content=output_data)
@@ -32,6 +33,27 @@ class Restructor_ii:
 
 
     """ add new features to the dataset """
+
+    def corrections_matching_data(self, item):
+        item["01.main_data"]["match_id"] = ""
+        item["01.main_data"]["match_level"] = ""
+
+    def corrections_unify_city(self, item):
+        translation_dict = {
+            "Bialystok"     : "Białystok",
+            "Cracow"        : "Kraków",
+            "Gdansk"        : "Gdańsk" ,
+            "Lodz"          : "Łódź",
+            "Nowy Sacz"     : "Nowy Sącz",
+            "Poznan"        : "Poznań",
+            "Rzeszow"       : "Rzeszów",
+            "Torun"         : "Toruń",
+            "Warsaw"        : "Warszawa",
+            "Wroclaw"       : "Wrocław",
+            "Zielona Gora"  : "Zielona Góra"
+        }
+        if item["02.location_details"]["city"] in translation_dict.keys():
+            item["02.location_details"]["city"] = translation_dict[item["02.location_details"]["city"]]
 
 
     def add_matching_data(self, input_file_name="datasets/combined_data.json", output_fle_name="datasets/combined_data_match.json"):
@@ -42,15 +64,16 @@ class Restructor_ii:
             print(" *** ")
             print(data[item]["01.main_data"]["name"])
 
-            data[item]["01.main_data"]["match_id"] = data[item]["01.main_data"]["id"]
-            data[item]["01.main_data"]["match_level"] = "self"
-
             if data[item]["01.main_data"]["source"] == "bj" and data[item]["01.main_data"]["type"] == "lease":
+
+                data[item]["01.main_data"]["match_id"] = data[item]["01.main_data"]["id"]
+                data[item]["01.main_data"]["match_level"] = "self"
 
                 m = 0
                 for com_item in data:
-                    if data[com_item]["01.main_data"]["source"] != "bj":
-                        similarity_level = self.similarity(data[item]["01.main_data"]["name"].replace(" ", "").replace("Business",""), data[com_item]["01.main_data"]["name"].replace(" ", "").replace("Business",""))
+                    if data[com_item]["01.main_data"]["source"] != "bj" and data[item]["01.main_data"]["type"] == "lease" and data[item]["'02.location_details'"]["city"] == data[com_item]["'02.location_details'"]["city"]:
+                        similarity_level = self.similarity(data[item]["01.main_data"]["name"], data[com_item]["01.main_data"]["name"])
+
                         if similarity_level > 0.7:
 
 
@@ -64,15 +87,15 @@ class Restructor_ii:
                                 else:
                                     pass
 
-                            # print(data[com_item]["01.main_data"])
-                            # print(similarity_level)
+                            print(data[com_item]["01.main_data"])
+                            print(similarity_level)
 
-                            # m += 1
-                            # if m >= 5:
-                            #     break
-            # sleep(0.025)
+                            m += 1
+                            if m >= 5:
+                                break
+            sleep(0.025)
 
-        save_json_file(file_name=output_fle_name, content=data)
+        # save_json_file(file_name=output_fle_name, content=data)
 
     # def add_match_data(self, file_name, output_file):
     #     output_data = []
@@ -106,8 +129,14 @@ class Restructor_ii:
     #             if n == 5:
     #                 break
 
+    # def similarity(self, a, b):
+    #     return SequenceMatcher(None, a, b).ratio()
+    #
+    # def similarity(self, a, b):
+    #     return SequenceMatcher(None, a.replace(" ", "").replace("Business",""), b.replace(" ", "").replace("Business","")).ratio()
+
     def similarity(self, a, b):
-        return SequenceMatcher(None, a, b).ratio()
+        return jellyfish.jaro_distance(a, b)
 
 
     """ debug features """
@@ -119,7 +148,7 @@ class Restructor_ii:
         n = 0
         for key in data:
             if int(key) in range(no_of_start_rec, no_of_end_rec):
-                print(data[key]['01.main_data'])
+                print(data[key])
                 n += 1
                 if n >= iteration_limit:
                     break
@@ -128,9 +157,9 @@ class Restructor_ii:
 
 if __name__ == "__main__":
     r = Restructor_ii()
-    # r.save_combined_json_data()
+    r.save_combined_json_data()
 
-    r.add_matching_data()
+    # r.add_matching_data()
 
-    # r.check_records(file_name="datasets/combined_data.json", no_of_start_rec=2400)
+    r.check_records(file_name="datasets/combined_data.json", no_of_start_rec=1)
 
